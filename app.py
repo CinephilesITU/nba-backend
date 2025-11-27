cd5b6c0abf3a47493f6ab6d03d9f4038ec953362
Mehmet İlyas İNCE <incem21@itu.edu.tr>
2025-11-27 23:59:13 +0300

    Fix formatting of app.run parameters

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def get_complex_stats():
 
 if __name__ == '__main__':
     # Node.js backend ile ayni portta (5001) calistiriyoruz
-    app.run(debug=True,host:"0.0.0.0",port=5001)
+    app.run(debug=True,host="0.0.0.0",port=5001)
